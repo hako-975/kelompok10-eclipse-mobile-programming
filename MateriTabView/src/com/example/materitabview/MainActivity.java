cbaedda372d109417d73840a52b2c11abd9db302
Andri Firman Saputra <andrifirmansaputra1@gmail.com>
2023-05-09 04:25:43 +0700

    materitabview

--- MateriTabView/src/com/example/materitabview/MainActivity.java
+++ MateriTabView/src/com/example/materitabview/MainActivity.java
@@ -1,15 +1,27 @@
 package com.example.materitabview;
 
+import android.app.TabActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.app.Activity;
 import android.view.Menu;
+import android.widget.TabHost; 
 
-public class MainActivity extends Activity {
+public class MainActivity extends TabActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        Intent intent;
+        TabHost tabHost = getTabHost();
+        TabHost.TabSpec tabSpec;
+        intent = new Intent(this, MenghitungHurufActivity.class);
+        tabSpec = getTabHost().newTabSpec("tab1").setIndicator("Menghitung Huruf").setContent(intent);
+        tabHost.addTab(tabSpec);
+        intent = new Intent(this, MenghitungKataActivity.class);
+        tabSpec = getTabHost().newTabSpec("tab2").setIndicator("Menghitung Kata").setContent(intent);
+        tabHost.addTab(tabSpec); 
     }
 
     @Override
